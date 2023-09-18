4b5131598dd72847841e653aac67952dd075de39
YeremiaHS <yeremia2405@gmail.com>
2023-09-18 15:27:07 +0700

    fix responsive details page & basic js home page

--- latihan-replika/scripts/home.js
+++ latihan-replika/scripts/home.js
@@ -7,14 +7,14 @@ const description = document.getElementById("description");
 
 const btnSubmit = document.getElementById("button-submit");
 
-btnSubmit.addEventListener("clicl", (e)=> {
+btnSubmit.addEventListener("click", (e)=> {
   e.preventDefault();
 
-  if (image.value === null) {
+  if (image.value === " ") {
     alert("please input url image");
-  }else if (title.value === null) {
+  }else if (title.value === " ") {
     alert("please input book title");
-  } else if (description.value === null) {
+  } else if (description.value === " ") {
     alert("please input book description");
   } else {
     alert("success add book!");
