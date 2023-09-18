8b0e2ab901dd5520dc4eaa29054148dba6e20d0d
YeremiaHS <yeremia2405@gmail.com>
2023-09-18 18:11:43 +0700

    fix home responsive modal & validation input

--- latihan-replika/scripts/home.js
+++ latihan-replika/scripts/home.js
@@ -10,11 +10,11 @@ const btnSubmit = document.getElementById("button-submit");
 btnSubmit.addEventListener("click", (e)=> {
   e.preventDefault();
 
-  if (image.value === " ") {
+  if (image.value === "") {
     alert("please input url image");
-  }else if (title.value === " ") {
+  }else if (title.value === "") {
     alert("please input book title");
-  } else if (description.value === " ") {
+  } else if (description.value === "") {
     alert("please input book description");
   } else {
     alert("success add book!");
